c8030d07337bfc70f2f157d5ae59ec6ac86cb162
Foysal Hasib <118272136+foysalhasib@users.noreply.github.com>
2024-09-28 22:06:11 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let one = document.querySelector(".one")
+
+
+one.addEventListener("mouseenter",function(){
+     one.style.background = "red"
+})
+one.addEventListener("mouseleave",function(){
+     one.style.background = "green"
+})
